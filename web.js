b417345c0c42299c58a3d7704b100255a158e4ff
Sebastien Bencherqui <sebastien.bencherqui@essec.edu>
2013-07-11 14:15:52 +0000

    try2

--- web.js
+++ web.js
@@ -2,7 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', fs.readFileSync('./index.html', 'utf8'));;
+var text = fs.readFileSync('index.html','utf8')
+
+console.log (text)
+
+app.get('/', function(request, response) {
+  response.send('Hello World2!');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
